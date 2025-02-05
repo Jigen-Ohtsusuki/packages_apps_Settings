0ddd17f86488675aeced2603ada0e57dbf7c99c0
Tequila Extended <tequila.extended@gmail.com>
2025-02-05 20:27:13 +0000

    Revert "Settings: Integrate crDroidSettings into Settings"
    This reverts commit de9860bad94098ff41c3b240f1509c1837927f9b.

--- src/com/android/settings/Settings.java
+++ src/com/android/settings/Settings.java
@@ -516,13 +516,4 @@ public class Settings extends SettingsActivity {
      * Activity for OneHandedSettings
      */
     public static class OneHandedSettingsActivity extends SettingsActivity { /* empty */ }
-
-    public static class PreviouslyConnectedDeviceActivity extends SettingsActivity { /* empty */ }
-
-    public static class ScreenTimeoutActivity extends SettingsActivity { /* empty */ }
-
-    /** Activity for the Reset mobile network settings. */
-    public static class ResetMobileNetworkSettingsActivity extends SettingsActivity { /* empty */ }
-
-    public static class crDroidSettingsLayoutActivity extends SettingsActivity {}
 }
